15a5a333d5b62df25dbb54b9da2df41cc20856d3
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-07-27 23:45:12 +0300

    Initial commit

---
+++ src/main/java/org/makar/spring3training/Spring3TrainingApplication.java
@@ -0,0 +1,13 @@
+package org.makar.spring3training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3TrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3TrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/makar/spring3training/Spring3TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.makar.spring3training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3TrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
