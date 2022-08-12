f592c6363a4d04b421f428dd86182320012e8dac
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-08-13 01:29:47 +0300

    deployment and service for k8s

--- src/main/java/org/makar/randomquotes/common/entity/Quote.java
+++ src/main/java/org/makar/randomquotes/common/entity/Quote.java
@@ -15,7 +15,7 @@ public class Quote implements Serializable {
     @EqualsAndHashCode.Exclude
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 1000)
     private String quote;
 
     @ManyToOne
